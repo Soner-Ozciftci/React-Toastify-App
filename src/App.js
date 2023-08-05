ff6b43cf6266115da68127a2079e7a4958fd3f2b
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-06 00:29:13 +0200

    password

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import "./App.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function notify() {
-  toast("Password Wrong!");
+  toast.error("🦄 Wrong Password!", {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "colored",
+  });
 }
 
 function App() {
   return (
     <div className="App">
       <button onClick={notify}>Notify!</button>
-      <ToastContainer />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </div>
   );
 }
