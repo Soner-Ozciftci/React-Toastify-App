1c6e0354e1832ac1500aa2ce6ba9a40200603829
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-06 00:14:49 +0200

    button add

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import "./App.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+function notify() {
+  toast("Password Wrong!");
+}
+
 function App() {
   return (
     <div className="App">
-      <button onClick={notify}></button>
+      <button onClick={notify}>Notify!</button>
       <ToastContainer />
     </div>
   );
